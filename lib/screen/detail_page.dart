370882dcb59910c9c257674b7e0887474a5de3db
heCollageStudent <ramadhandhirsya@gmail.com>
2024-09-25 11:31:51 +0700

    add features search & list item

--- lib/screen/detail_page.dart
+++ lib/screen/detail_page.dart
@@ -1,1 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:the_zodiacs/model/data_zodiacs.dart';
 
+class DetailPage extends StatelessWidget {
+  final Zodiacs sign;
+
+  const DetailPage({super.key, required this.sign});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      if (constraints.maxWidth > 800) {
+        return DetailWebPage(sign: sign);
+      } else {
+        return DetailMobilePage(sign: sign);
+      }
+    });
+  }
+}
+
+class DetailWebPage extends StatefulWidget {
+  final Zodiacs sign;
+
+  const DetailWebPage({super.key, required this.sign});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final Zodiacs sign;
+
+  const DetailMobilePage({super.key, required this.sign});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Stack(
+                children: [
+                  ExpandableCarousel(
+                    options: ExpandableCarouselOptions(
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 2),
+                    ),
+                    items: [1, 2, 3, 4, 5].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(color: Colors.amber),
+                              child: Text(
+                                'text $i',
+                                style: TextStyle(fontSize: 16.0),
+                              ));
+                        },
+                      );
+                    }).toList(),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,16 +1,229 @@
 import 'package:flutter/material.dart';
+import 'package:the_zodiacs/model/data_zodiacs.dart';
+import 'package:the_zodiacs/screen/detail_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: ZodiacSearchDelegate(),
+                );
+              },
+            ),
+          ],
+        ),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth <= 600) {
+              return const TourismPlaceList();
+            } else if (constraints.maxWidth <= 1200) {
+              return const TourismPlaceGrid(gridCount: 4);
+            } else {
+              return const TourismPlaceGrid(gridCount: 6);
+            }
+          },
+        ),
+      );
+    });
+  }
+}
+
+class ZodiacSearchDelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
       ),
-      body: const Center(
-        child: Text('Welcome to the Home Page!'),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final results = zodiacsData
+        .where(
+            (zodiac) => zodiac.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        final Zodiacs sign = results[index];
+        return Card(
+          child: ListTile(
+            leading: Image.asset(
+              sign.imgAsset,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+            title: Text(sign.name),
+            subtitle: Text(sign.birthdate),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(sign: sign);
+              }));
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = zodiacsData
+        .where(
+            (zodiac) => zodiac.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final Zodiacs sign = suggestions[index];
+        return ListTile(
+          title: Text(sign.name),
+          onTap: () {
+            query = sign.name;
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  final int gridCount;
+
+  const TourismPlaceGrid({super.key, required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        children: zodiacsData.map((sign) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(sign: sign);
+              }));
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      sign.imgAsset,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      sign.name,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(
+                      sign.birthdate,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          final Zodiacs sign = zodiacsData[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(sign: sign);
+              }));
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(sign.imgAsset),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            sign.name,
+                            style: const TextStyle(fontSize: 16.0),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(sign.birthdate),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: zodiacsData.length,
       ),
     );
   }
